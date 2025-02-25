972a9e171a3b1fa33c18b6a88d307ada4e66e13f
Saipriya Madapati <saipriyamadapati@DBT000494.local>
2025-02-25 09:14:12 +0000

    removed healthcheck

--- cleanup_atlassian.py
+++ cleanup_atlassian.py
@@ -12,10 +12,6 @@ from dateutil.tz import UTC
 from furl import furl
 from uplink.auth import BearerToken
 
-import requests
-
-from flask import Flask
-
 app = Flask(__name__)
 
 # Set up logging (ECS)
@@ -213,20 +209,5 @@ def cleanup(
             f'Error: Atlassian organisation "{organisation_name}" not found'
         )
 
-def healthcheck():
-    app_base_url = APP_BASE_URL = os.environ["BASE_URL"]
-    response = requests.get(app_base_url/healthcheck)
-    if response.status_code == 200:
-        return "OK"
-    else:
-        return "Health check failed!!"
-    
-@app.route('/healthcheck', methods=['GET'])
-def healthcheck_endpoint():
-    return healthcheck()
-
 if __name__ == "__main__":
-    fire.Fire({
-        'cleanup': cleanup,
-        'health_check': healthcheck
-        })
+    fire.Fire(cleanup)
